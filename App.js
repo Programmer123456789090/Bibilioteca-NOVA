7e77ea2f5208956b5d8d99a49fd049bba2621b2d
Programmer123456789090 <112125291+Programmer123456789090@users.noreply.github.com>
2023-09-21 15:40:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import BottomTabNavigator from "./components/BottomTabNavigator" ;
+import React, { Component } from "react";
+export default class  App extends Component{
+  render () {
+    return (
+      <BottomTabNavigator/>
+    );
+  }
+  
+  
+}
+
+
